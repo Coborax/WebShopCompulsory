44a4e7c0eb55a0ee7e72972fe4b99b9bb44b5a56
Kjell Schoke <kjell.gudrun-schoke@hotmail.com>
2021-11-16 00:01:35 +0100

    Update InterfaceProductServiceTest.cs
    Added test case for create and delete method in InterfaceProductServiceTest

--- Backend/WebShop.Core.Tests/IServices/InterfaceProductServiceTest.cs
+++ Backend/WebShop.Core.Tests/IServices/InterfaceProductServiceTest.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Reflection.Metadata;
 using Moq;
 using WebShop.Core.IServices;
 using WebShop.Core.Models;
@@ -9,14 +10,30 @@ namespace WebShop.Core.Tests.IServices
     public class InterfaceProductServiceTest
     {
         [Fact]
-        public void InterfaceProductServiceIsAvailable()
+        public void InterfaceProductService_IsAvailable()
         {
             var service = new Mock<IProductService>().Object;
             Assert.NotNull(service);
         }
 
+        /// <summary>
+        /// Tests whether product service returns product when the product is created
+        /// </summary>
         [Fact]
-        public void GetProductWithNoParamsReturnsListOfAllProducts()
+        public void Create_ValidProduct_ReturnsCreatedProduct()
+        {
+            var mock = new Mock<IProductService>();
+            var expected = new Product();
+            
+            mock.Setup(s => s.Create(expected))
+                .Returns(expected);
+            var service = mock.Object;
+            
+            Assert.Equal(expected, service.Create(expected));
+        }
+
+        [Fact]
+        public void Get_ProductWithNoParams_ReturnsListOfAllProducts()
         {
             var mock = new Mock<IProductService>();
             var fakeList = new List<Product>();
@@ -25,6 +42,22 @@ namespace WebShop.Core.Tests.IServices
             var service = mock.Object;
             
             Assert.Equal(fakeList, service.GetAll());
+        }
+
+        /// <summary>
+        /// Tests whether product service returns product when the product is deleted
+        /// </summary>
+        [Fact]
+        public void Delete_ValidProduct_ReturnsDeletedProduct()
+        {
+            var mock = new Mock<IProductService>();
+            var expected = new Product();
+            var service = mock.Object;
+
+            mock.Setup(s => s.Delete(expected))
+                .Returns(expected);
+            
+            Assert.Equal(expected, service.Delete(expected));
         }
     }
 }
